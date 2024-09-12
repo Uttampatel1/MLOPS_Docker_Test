6816eefbf0d04cc1aa181927699299394081d222
Uttam Patel <uttam.pipaliya@topialifesciences.com>
2024-09-12 14:28:29 +0530

    Update UI

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
 def test_home():
-    response = app.test_client().get("/")
+    response = app.test_client().get("/test")
     
     assert response.status_code ==200
     assert response.data == b"Hello, World!"
